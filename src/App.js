00933718ea95bb8eb9b70763885312ed1df86f2d
Farhan-Abid <shahabid628@gmail.com>
2022-03-26 02:20:16 +0600

    changed header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <h1 className='mt-3 movie-text-color'>MOVIES SELECTION MANIA</h1>
     </div>
   );
 }
